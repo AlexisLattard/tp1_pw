c9ba55dbd3dd0dd5d846c77573b56e25bb720aa7
Abyssion <alexis@lattard.fr>
2018-04-04 13:25:16 +0200

    Menu home et searchbar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+export interface Result {
+  title: string;
+  author: string;
+  date: string;
+  image: string;
+}
+
+const fakeResult: Result[] = [
+  { title: 'Michel', author: 'Notre ami Michel !', date: '14/02/1953', image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Michel_Cremades.jpg/220px-Michel_Cremades.jpg"},
+  { title: 'RRRRRRRRRRRr', author: 'Alain Chabat', date: '15/12/1984', image: 'https://media.senscritique.com/media/000004673929/1200/RRRrrrr.jpg'},
+  { title: 'Michel', author: 'Notre ami Michel !', date: '14/02/1953', image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Michel_Cremades.jpg/220px-Michel_Cremades.jpg" },
+  { title: 'RRRRRRRRRRRr', author: 'Alain Chabat', date: '15/12/1984', image: 'https://media.senscritique.com/media/000004673929/1200/RRRrrrr.jpg' },
+  { title: 'Michel', author: 'Notre ami Michel !', date: '14/02/1953', image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Michel_Cremades.jpg/220px-Michel_Cremades.jpg" },
+  { title: 'RRRRRRRRRRRr', author: 'Alain Chabat', date: '15/12/1984', image: 'https://media.senscritique.com/media/000004673929/1200/RRRrrrr.jpg' }
+]
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  results: Result[];
 
   constructor(public navCtrl: NavController) {
-
+    this.results = fakeResult;
   }
 
+  getLength(event: any){
+    return event.length;
+
+
+  }
 }
